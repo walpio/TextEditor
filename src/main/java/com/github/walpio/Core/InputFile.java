b8f3cb0fde2fb433bdcfb82705cd6fe4bf518a3c
walpio <doksmal@gmail.com>
2018-01-17 00:06:25 +0100

    splitting text into sentences

--- src/main/java/com/github/walpio/Core/InputFile.java
+++ src/main/java/com/github/walpio/Core/InputFile.java
@@ -3,6 +3,9 @@ package com.github.walpio.Core;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputFile {
 
@@ -19,5 +22,19 @@ public class InputFile {
             line = bufferedReader.readLine();
         }
         return inputText;
+    }
+
+    public List<String> splitFileIntoSentences() throws IOException {
+        BreakIterator breakIterator = BreakIterator.getSentenceInstance();
+        List<String> sentences = new ArrayList<String>();
+
+        String inputText = readFile();
+        breakIterator.setText(inputText);
+        int start = breakIterator.first();
+        for (int end = breakIterator.next(); end != BreakIterator.DONE; start = end, end = breakIterator.next()) {
+            String sentence = inputText.substring(start, end).trim();
+            sentences.add(sentence);
+        }
+        return sentences;
     }
 }
