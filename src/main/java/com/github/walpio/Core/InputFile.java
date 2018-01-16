a1a44a6aed3cb88c13b32cd7f1fa09f8389a0847
walpio <doksmal@gmail.com>
2018-01-16 23:26:06 +0100

    reading sample text file

--- src/main/java/com/github/walpio/Core/InputFile.java
+++ src/main/java/com/github/walpio/Core/InputFile.java
@@ -1,4 +1,23 @@
 package com.github.walpio.Core;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class InputFile {
+
+    private String filePath = ".\\Sample\\Sample Text.txt";
+
+    public String readFile() throws IOException {
+        FileReader fileReader = new FileReader(filePath);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        String inputText = "";
+        String line = bufferedReader.readLine();
+        while (line != null) {
+            inputText += line;
+            line = bufferedReader.readLine();
+        }
+        return inputText;
+    }
 }
