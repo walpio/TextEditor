85cc5637440e88630f04e2726c84ac8db69e2e15
walpio <doksmal@gmail.com>
2018-01-17 23:40:17 +0100

    splitting sentences into words (map)

--- src/main/java/com/github/walpio/Core/InputFile.java
+++ src/main/java/com/github/walpio/Core/InputFile.java
@@ -4,8 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.BreakIterator;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class InputFile {
 
@@ -26,15 +25,43 @@ public class InputFile {
 
     public List<String> splitFileIntoSentences() throws IOException {
         BreakIterator breakIterator = BreakIterator.getSentenceInstance();
-        List<String> sentences = new ArrayList<String>();
+        List<String> sentences = new ArrayList<>();
 
         String inputText = readFile();
         breakIterator.setText(inputText);
         int start = breakIterator.first();
-        for (int end = breakIterator.next(); end != BreakIterator.DONE; start = end, end = breakIterator.next()) {
+        for (int end = breakIterator.next();
+             end != BreakIterator.DONE;
+             start = end, end = breakIterator.next()) {
             String sentence = inputText.substring(start, end).trim();
             sentences.add(sentence);
         }
         return sentences;
+    }
+
+    public Map<String, List<String>> splitSentencesIntoWords() throws IOException {
+        Map<String, List<String>> sortedWords = new LinkedHashMap<>();
+
+        for (int i = 0; i < splitFileIntoSentences().size(); i++) {
+            String sentence = splitFileIntoSentences().get(i);
+            String key = String.format("Sentence %d", (i + 1));
+            List<String> listOfWords = Arrays.asList(sentence.split("\\s+"));
+            Collections.sort(listOfWords, String.CASE_INSENSITIVE_ORDER);
+            sortedWords.put(key, listOfWords);
+        }
+        return sortedWords;
+    }
+
+    public List<List<String>> splitSentencesIntoListOfWords() throws IOException {
+        List<List<String>> sortedWords = new ArrayList<>();
+
+        String sentence;
+        for (int i = 0; i < splitFileIntoSentences().size(); i++) {
+            sentence = splitFileIntoSentences().get(i);
+            List<String> listOfWords = Arrays.asList(sentence.split("\\s+"));
+            Collections.sort(listOfWords, String.CASE_INSENSITIVE_ORDER);
+            sortedWords.add(listOfWords);
+        }
+        return sortedWords;
     }
 }
