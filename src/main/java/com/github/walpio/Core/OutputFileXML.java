7dc6ce59518dd5f8b4917030f08cec407510706c
walpio <doksmal@gmail.com>
2018-02-01 22:41:48 +0100

    Creating XML file added

--- src/main/java/com/github/walpio/Core/OutputFileXML.java
+++ src/main/java/com/github/walpio/Core/OutputFileXML.java
@@ -1,7 +1,27 @@
 package com.github.walpio.Core;
 
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class OutputFileXML {
 
-    public void writeToXML() {
+    public static void main(String[] args) throws IOException {
+        writeToXML();
+    }
+
+    public static void writeToXML() throws IOException {
+        InputFile inputFile = new InputFile();
+        Document doc = new Document();
+        Element theRoot = new Element("text");
+        doc.setRootElement(theRoot);
+        XMLOutputter xmlOutput = new XMLOutputter(Format.getPrettyFormat());
+        xmlOutput.output(doc, new FileOutputStream(new File(".\\Sample\\OutputXML.xml")));
+        System.out.println("XML created");
     }
 }
